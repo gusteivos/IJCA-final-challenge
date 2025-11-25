c879fdb3a9fc33036a32b398a95f5ad3705bb820
Gustavo Ramos Carvalho <gc5142387@gmail.com>
2025-11-25 19:40:47 -0300

    ETL finalizado

--- Transform_acidentes.py
+++ Transform_acidentes.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
 
+import glob
+
+acidentes_csvs = glob.glob("medallion/bronze/acidentes*.csv")
 
 
 def transform_acidentes():
-    df_acidentes = pd.read_csv("medallion/bronze/acidentes2025.csv", sep=';', encoding="latin1", index_col="id")
+    df_acidentes = pd.concat((pd.read_csv(acidentes, sep=';', encoding="latin1", index_col="id") for acidentes in acidentes_csvs), ignore_index=True)
     df_acidentes.info()
 
 
@@ -19,7 +22,20 @@ def transform_acidentes():
     df_acidentes = df_acidentes.rename(columns={"horario": "hora"})
     df_acidentes.info()
 
+    print(df_acidentes.isna().sum())
+    df_acidentes["classificacao_acidente"] = df_acidentes["classificacao_acidente"].fillna("Não Informado")
 
+    df_acidentes["tipo_veiculo"] = df_acidentes["tipo_veiculo"].fillna("Não Informado")
+
+    df_acidentes["tipo_envolvido"] = df_acidentes["tipo_envolvido"].fillna("Não Informado")
+    df_acidentes["estado_fisico"] = df_acidentes["estado_fisico"].fillna("Não Informado")
+
+    df_acidentes["sexo"] = df_acidentes["sexo"].fillna("Não Informado")
+
+    df_acidentes["regional"] = df_acidentes["regional"].fillna("Não Informado")
+    df_acidentes["delegacia"] = df_acidentes["delegacia"].fillna("Não Informado")
+    df_acidentes["uop"] = df_acidentes["uop"].fillna("Não Informado")
+    print(df_acidentes.isna().sum())
     df_acidentes.to_csv("medallion/silver/acidentes.csv", index=False)
 
 
--- Transform_datatran.py
+++ Transform_datatran.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
 
+import glob
+
+datatran_csvs = glob.glob("medallion/bronze/datatran*.csv")
 
 
 def transform_datatran():
-    df_datatran = pd.read_csv("medallion/bronze/datatran2025.csv", sep=';', encoding="latin1", index_col="id")
+    df_datatran = pd.concat((pd.read_csv(datatran, sep=';', encoding="latin1", index_col="id") for datatran in datatran_csvs), ignore_index=True)
     df_datatran.info()
 
 
@@ -18,6 +21,14 @@ def transform_datatran():
 
     df_datatran = df_datatran.rename(columns={"horario": "hora"})
     df_datatran.info()
+
+    print(df_datatran.isna().sum())
+    df_datatran["classificacao_acidente"] = df_datatran["classificacao_acidente"].fillna("Não Informado")
+
+    df_datatran["regional"] = df_datatran["regional"].fillna("Não Informado")
+    df_datatran["delegacia"] = df_datatran["delegacia"].fillna("Não Informado")
+    df_datatran["uop"] = df_datatran["uop"].fillna("Não Informado")
+    print(df_datatran.isna().sum())
 
 
     df_datatran.to_csv("medallion/silver/datatran.csv", index=False)
